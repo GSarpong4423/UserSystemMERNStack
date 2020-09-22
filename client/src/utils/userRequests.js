927a541d83af26f217352a8a8d77dcbc9d85807c
GSarpong4423 <sarpongg@gmail.com>
2020-09-21 20:58:01 -0400

    updated userrequest

--- client/src/utils/userRequests.js
+++ client/src/utils/userRequests.js
@@ -1,59 +1,83 @@
 const {default: axios} = require('axios');
+const {default: validate} = require('validator')
 const baseURL = 'http://localhost:4000';
+const isEmpty = require('./isEmpty');
 
 module.exports = {
-  loginReq: (form) => {
-
-    const reqBody = {};
-
+  loginReq: async (form) => {
+    const 
+      reqBody = {},
+      failedValues = [];
     for (const input of form) {
-      
-      const val = input.value;
-
-      if (val !== '') {
+      const val = input.value.trim();
+      if (!isEmpty(val)) {
         reqBody[input.name] = val
-      }
-    
-    }
+        switch (input.name) {
+          case 'credential':
+              if ( !validate.isEmail(val) ) {
+                const username = reqBody.credential;
+                if ( !validate.isAscii(username) ) failedValues.push({ key: 'username', error: 'Username Using Invalid Characters'})
+                if ( username.length < 3 ) failedValues.push({ key: 'username', error: 'Username Needs To Be At Least 3 Characters'})
+                if ( !username.length > 21 ) failedValues.push({ key: 'username', error: 'Username Needs To Be Shorter Than 21 Characters'})
+              } else {
+                const email = reqBody.email;
+                if ( !validate.isEmail(email)) failedValues.push({key: 'email', error: 'Email Invalid'})
+                if ( email.length < 7 ) failedValues.push({key: 'email', error: 'Email Needs To Be At Least 7 Characters'})
+                if ( email.length > 255 ) failedValues.push({key: 'email', error: 'Email Needs To Be Shorter Than 256 Characters'})
+                if ( !validate.isAscii(email)) failedValues.push({key: 'email', error: 'Email Is Using Invalid Characters'})      
+              }
+            break;
+          case 'password':
+            const password = reqBody.password;
+                if ( password.length < 7 ) failedValues.push({key: 'password', error: 'Password Needs To Be At Least 7 Characters'})
+                if ( password.length > 1000 ) failedValues.push({key: 'password', error: 'Password Needs To Be Shorter Than 1000 Characters'})
+            break;  
+          default:       
+        }
+      } else {
 
-    //frontend validation needed:
-    // [] make sure all feilds are present,
-    if (Object.keys(reqBody).length !== 2) {
-      return alert('You forgot to fill something out')
-    } 
-    // [] lengths of inputs (match schema)
+        failedValues.push({key: input.name, error: `${input.name} Is Required`})
+
+      }
+    }
+    
+    if (failedValues.length !== 0) {
+      return alert(failedValues.map(err=>{return `Error with ${err.key}:\n${err.error}\n`}).join('\n'))
+    }
 
     const loginURL = `${baseURL}/user/login`; 
 
-    fetch(loginURL, {
-        method: "PUT",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(reqBody)
-    })
-    .then(rs => { 
-      console.log(rs);
-      return rs.json()
-    })
-    .then(res => {
-      console.log(res)
-    })
-
-    // axios.put(loginURL, reqBody)
-    // .then( res => {
-    //   console.log(res);
+    // return fetch(loginURL, {
+    //     method: "PUT",
+    //     mode: "cors",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify(reqBody)
     // })
-    // .catch( err => {
-    //   if (err) {
-    //     console.log(err);
+    // .then(rs => { 
+    //   console.log(rs);
+      
+    //   if (rs.status === 200) {
+    //     alert("You've successully logged in")
     //   }
+
+    //   return rs.json()
+    // })
+    // .then(res => {
+    //   console.log(res)
+      
+    // })
+    // .finally( (res) => {
+    //   return res.username
     // })
 
+    const user = await axios.put(loginURL, reqBody).then(response => response.data)
+    
+    return user
   },
 
-  regReq: (form) => {
+  regReq: async (form) => {
 
     const reqBody = {};
 
@@ -81,7 +105,7 @@ module.exports = {
 
     const regURL = `${baseURL}/user/register`; 
 
-    axios.post(regURL, reqBody)
+    const user = await axios.post(regURL, reqBody)
     .then( res => {
       console.log(res);
     })
@@ -90,5 +114,6 @@ module.exports = {
         console.log(err); 
       }
     })
+    return user
   }
 }
